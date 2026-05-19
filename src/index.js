4cda12087e97c1465a22efef0eb66834f66f5f21
agonzalezbridge <asier.medina.ugalde@gmail.com>
2026-05-19 15:40:42 +0200

    proyecto dockerizado con mongo y postgres como bd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import sequelize from "./config/postgres.js";
+import connectMongo from "./config/mongo.js";
+
+dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando");
+  res.send("IsWorking API funcionando");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor en puerto 3000");
-});
+const startServer = async () => {
+  try {
+
+    await sequelize.authenticate();
+    console.log("PostgreSQL conectado");
+
+    await connectMongo();
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Servidor en puerto ${process.env.PORT || 3000}`);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
