3366ac8041886e1537f9644191ec10a42f9499c5
agonzalezbridge <asier.medina.ugalde@gmail.com>
2026-05-21 17:22:01 +0200

    rama para pruebas y añadido associations.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import connectMongo from './config/mongo.js'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.routes.js'
 import companyRouter from './routes/company.routes.js'
+import setupAssociations  from './models/postgres/associations.js'
+import { notFound, errorHandler } from './middlewares/errorHandler.js'
 const app = express()
-
+setupAssociations()
 
 dotenv.config()
 app.use(express.json())
@@ -29,3 +31,6 @@ const start = async () => {
 }
 
 start()
+
+app.use(notFound)
+app.use(errorHandler)
---
+++ src/models/postgres/associations.js
@@ -0,0 +1,89 @@
+import Company from './Company.js'
+import User from './User.js'
+
+
+
+export const setupAssociations = () => {
+  // Company -> Users
+  Company.hasMany(User, {
+    foreignKey: 'company_id',
+    as: 'users',
+    onDelete: 'CASCADE'
+  })
+
+  User.belongsTo(Company, {
+    foreignKey: 'company_id',
+    as: 'company'
+  })
+
+  /*
+  // Company -> ShiftTemplates
+  Company.hasMany(ShiftTemplate, {
+    foreignKey: 'company_id',
+    as: 'shift_templates',
+    onDelete: 'CASCADE'
+  })
+
+  ShiftTemplate.belongsTo(Company, {
+    foreignKey: 'company_id',
+    as: 'company'
+  })
+
+  // User -> Schedules
+  User.hasMany(Schedule, {
+    foreignKey: 'user_id',
+    as: 'schedules',
+    onDelete: 'CASCADE'
+  })
+
+  Schedule.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'user'
+  })
+
+  // ShiftTemplate -> Schedules
+  ShiftTemplate.hasMany(Schedule, {
+    foreignKey: 'shift_template_id',
+    as: 'schedules'
+  })
+
+  Schedule.belongsTo(ShiftTemplate, {
+    foreignKey: 'shift_template_id',
+    as: 'shift_template'
+  })
+
+  // User creador -> Schedules
+  User.hasMany(Schedule, {
+    foreignKey: 'created_by',
+    as: 'created_schedules'
+  })
+
+  Schedule.belongsTo(User, {
+    foreignKey: 'created_by',
+    as: 'creator'
+  })
+
+  // User -> TimeRecords
+  User.hasMany(TimeRecord, {
+    foreignKey: 'user_id',
+    as: 'time_records',
+    onDelete: 'CASCADE'
+  })
+
+  TimeRecord.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'user'
+  })
+
+  // Schedule -> TimeRecords
+  Schedule.hasMany(TimeRecord, {
+    foreignKey: 'schedule_id',
+    as: 'time_records'
+  })
+
+  TimeRecord.belongsTo(Schedule, {
+    foreignKey: 'schedule_id',
+    as: 'schedule'
+  })
+  */
+}
