632fb42e8ba0667fc33b982b0f034dcab5497838
agonzalezbridge <asier.medina.ugalde@gmail.com>
2026-05-27 09:25:35 +0200

    cambios borrados por conflicto en rama

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        changeOrigin: true
+        changeOrigin: true,
+        credentials: true
       }
     }
   }
